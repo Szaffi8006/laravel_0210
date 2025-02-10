a0037f370d46245c4775413060dfc42160050a8e
Szaffi8006 <cska8006@gmail.com>
2025-02-10 19:10:41 +0100

    19.10

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\RegisterRequest;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
+    public function register(RegisterRequest $request){
 
-        //$request->validated();
+        $request->validated();
 
-        $user = User::create([
-            "name"=>$request ["name"],
-            "email"=>$request ["email"],
-            "password"=>bcrypt ($request ["password"]),
-        ]);
+        // $user = User::create([
+        //     "name"=>$request ["name"],
+        //     "email"=>$request ["email"],
+        //     "password"=>bcrypt ($request ["password"]),
+        // ]);
         return $user;
     }
 
